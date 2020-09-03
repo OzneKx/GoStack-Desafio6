b15e9c71f69efc1eb4829e990aaef16420d05ab3
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-09-02 22:08:49 -0300

    GoStack - Desafio 6
    Desafio 6 do Bootcamp GoStack

---
+++ src/database/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-unresolved */
+import { createConnection, getConnectionOptions, Connection } from 'typeorm';
+
+export default async (name = 'default'): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      name,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'gostack_desafio07_tests'
+          : defaultOptions.database,
+    }),
+  );
+};
